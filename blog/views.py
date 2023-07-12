aec674d9f62dae3da5db1a0cd1ec75139e8c45ee
Alexandr Litovchenko <zedlaa@gmail.com>
2023-07-12 22:11:06 +0300

    Добавил базовую вьюху

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def users(request: HttpRequest) -> HttpResponse:
+    user_list: list[User] = list(User.objects.all())
+
+    return render(request, 'blog/users.html', context={'user_list': user_list})
--- django_test_template/urls.py
+++ django_test_template/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blog.views import users
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', users),
 ]
